58feb4ffe53af9c12cb5e1f10b643b6e95e17b07
SatishK2022 <sk7399052@gmail.com>
2024-05-24 19:48:20 +0530

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,9 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-    const [dropdownOpen, setDropdownOpen] = useState(false);
-
-    const toggleDropdown = () => {
-        setDropdownOpen(!dropdownOpen);
-    };
+    const [travelMode, setTravelMode] = useState(false)
+    const [login, setLogin] = useState(false);
+    const [register, setRegister] = useState(false)
 
     return (
         <nav className="bg-white shadow-md">
@@ -19,10 +17,10 @@ const Header = () => {
                         <li><Link to="/" className="text-gray-600 hover:text-gray-900">Home</Link></li>
 
                         <li className="relative">
-                            <button type="button" className="text-gray-600 w-28 hover:text-gray-900" onClick={toggleDropdown}>
+                            <button type="button" onClick={() => setTravelMode(!travelMode)} className="text-gray-600 w-28 hover:text-gray-900">
                                 Travel Mode <span className="right-0 text-gray-400">▾</span>
                             </button>
-                            {dropdownOpen && (
+                            {travelMode && (
                                 <ul className="absolute w-full bg-white text-gray-600 border mt-2 right-0">
                                     <li><Link to="/flightbookings" className="block px-5 py-2 hover:bg-gray-200">Flight</Link></li>
                                     <li><Link to="/trainbookings" className="block px-5 py-2 hover:bg-gray-200">Train</Link></li>
@@ -35,10 +33,10 @@ const Header = () => {
                         <li><Link to="/about" className="text-gray-600 hover:text-gray-900">About</Link></li>
                         <li><Link to="/contact" className="text-gray-600 hover:text-gray-900">Contact</Link></li>
                         <li className="relative">
-                            <button type="button" className="text-gray-600 w-28 hover:text-gray-900" onClick={toggleDropdown}>
+                            <button type="button" className="text-gray-600 w-28 hover:text-gray-900" onClick={() => setLogin(!login)}>
                                 Login <span className="right-0 text-gray-400">▾</span>
                             </button>
-                            {dropdownOpen && (
+                            {login && (
                                 <ul className="absolute w-full bg-white text-gray-600 border mt-2 right-0">
                                     <li><Link to="/venderlogin" className="block px-5 py-2 hover:bg-gray-200">Vender</Link></li>
                                     <li><Link to="/corporatelogin" className="block px-5 py-2 hover:bg-gray-200">Corporate</Link></li>
@@ -47,10 +45,10 @@ const Header = () => {
                             )}
                         </li>
                         <li className="relative">
-                            <button type="button" className="text-gray-600 w-28 hover:text-gray-900" onClick={toggleDropdown}>
+                            <button type="button" className="text-gray-600 w-28 hover:text-gray-900" onClick={() => setRegister(!register)}>
                                 Register <span className="right-0 text-gray-400">▾</span>
                             </button>
-                            {dropdownOpen && (
+                            {register && (
                                 <ul className="absolute w-full bg-white text-gray-600 border mt-2 right-0">
                                     <li><Link to="/vender" className="block px-5 py-2 hover:bg-gray-200">Vender</Link></li>
                                     <li><Link to="/retail" className="block px-5 py-2 hover:bg-gray-200">Retail</Link></li>
