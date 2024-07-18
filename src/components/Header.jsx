50ea1d2b3132fe34a2e78769e0c29bf92e0a7108
SatishK2022 <sk7399052@gmail.com>
2024-07-18 21:13:08 +0530

    update navbar dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -38,12 +38,12 @@ const Header = () => {
                     <ul className="flex space-x-6">
                         <li><Link to="/" className="text-gray-600 hover:text-gray-900">Home</Link></li>
 
-                        <li className="relative">
-                            <button type="button" onClick={() => setTravelMode(!travelMode)} className="text-gray-600 w-28 hover:text-gray-900">
+                        <li className="relative" onMouseEnter={() => setTravelMode(true)} onMouseLeave={() => setTimeout(() => setTravelMode(false), 100)}>
+                            <button type="button" className="text-gray-600 w-28 hover:text-gray-900">
                                 Travel Mode <span className="right-0 text-gray-400">▾</span>
                             </button>
                             {travelMode && (
-                                <ul className="absolute w-full bg-white text-gray-600 border mt-2 right-0">
+                                <ul className="absolute w-full bg-white text-gray-600 border pt-2 right-0">
                                     <li><Link to="/flightbookings" className="block px-5 py-2 hover:bg-gray-200">Flight</Link></li>
                                     <li><Link to="/trainbookings" className="block px-5 py-2 hover:bg-gray-200">Train</Link></li>
                                     <li><Link to="/cabbookings" className="block px-5 py-2 hover:bg-gray-200">Cab</Link></li>
@@ -54,20 +54,20 @@ const Header = () => {
                         <li><Link to="#" className="text-gray-600 hover:text-gray-900">Destinations</Link></li>
                         <li><Link to="/about" className="text-gray-600 hover:text-gray-900">About</Link></li>
                         <li><Link to="/contact" className="text-gray-600 hover:text-gray-900">Contact</Link></li>
-                        <li className="relative">
-                            <button type="button" onClick={() => setServices(!services)} className="text-gray-600 w-28 hover:text-gray-900">
+                        <li className="relative" onMouseEnter={() => setServices(true)} onMouseLeave={() => setTimeout(() => setServices(false), 100)}>
+                            <button type="button" className="text-gray-600 w-28 hover:text-gray-900">
                                 Services <span className="right-0 text-gray-400">▾</span>
                             </button>
                             {services && (
-                                <ul className="absolute w-full bg-white text-gray-600 border mt-2 right-0">
+                                <ul className="absolute w-full bg-white text-gray-600 border pt-2 right-0">
                                     <li><Link to="/hotelbookings" className="block px-5 py-2 hover:bg-gray-200">Hotel</Link></li>
                                     <li><Link to="/passport" className="block px-5 py-2 hover:bg-gray-200">Passport</Link></li>
-                                    <li>
-                                        <button type="button" className="flex flex-row items-center justify-center px-5 py-2 hover:bg-gray-200 w-full" onClick={() => setInsurance(!insurance)}>
+                                    <li className="relative" onMouseEnter={() => setInsurance(true)} onMouseLeave={() => setTimeout(() => setInsurance(false), 100)}>
+                                        <button type="button" className="flex flex-row items-center justify-center px-5 py-2 hover:bg-gray-200 w-full">
                                             Insurance <span>&#9656;</span>
                                         </button>
                                         {insurance && (
-                                            <ul className="w-full absolute left-full top-20 bg-white text-gray-600 border">
+                                            <ul className="w-full absolute left-full -top-0.5 bg-white text-gray-600 border">
                                                 <li><Link to="/travelinsurance" className="flex w-full px-5 py-2 hover:bg-gray-200">Travel</Link></li>
                                                 <li><Link to="/healthlifeinsurance" className="flex px-5 py-2 hover:bg-gray-200">Health</Link></li>
                                             </ul>
@@ -84,13 +84,13 @@ const Header = () => {
                                     </button>
                                 </li>
                             ) : (
-                                <div className='flex'>
-                                    <li className="relative">
-                                        <button type="button" className="text-gray-600 w-28 hover:text-gray-900" onClick={() => setLogin(!login)}>
+                                <div className='flex items-center'>
+                                    <li className="relative" onMouseEnter={() => setLogin(true)} onMouseLeave={() => setTimeout(() => setLogin(false), 100)}>
+                                        <button type="button" className="text-gray-600 w-28 hover:text-gray-900">
                                             Login <span className="right-0 text-gray-400">▾</span>
                                         </button>
                                         {login && (
-                                            <ul className="absolute w-full bg-white text-gray-600 border mt-2 right-0">
+                                            <ul className="absolute w-full bg-white text-gray-600 border pt-2 right-0">
                                                 <li><Link to="/retail-login" className="block px-5 py-2 hover:bg-gray-200">Retail</Link></li>
                                                 <li><Link to="/corporate-login" className="block px-5 py-2 hover:bg-gray-200">Corporate</Link></li>
                                                 <li><Link to="/vendor-login" className="block px-5 py-2 hover:bg-gray-200">Vendor</Link></li>
@@ -98,12 +98,12 @@ const Header = () => {
                                             </ul>
                                         )}
                                     </li>
-                                    <li className="relative">
-                                        <button type="button" className="text-gray-600 w-28 hover:text-gray-900" onClick={() => setRegister(!register)}>
+                                    <li className="relative" onMouseEnter={() => setRegister(true)} onMouseLeave={() => setTimeout(() => setRegister(false), 100)}>
+                                        <button type="button" className="text-gray-600 w-28 hover:text-gray-900">
                                             Register <span className="right-0 text-gray-400">▾</span>
                                         </button>
                                         {register && (
-                                            <ul className="absolute w-full bg-white text-gray-600 border mt-2 right-0">
+                                            <ul className="absolute w-full bg-white text-gray-600 border pt-2 right-0">
                                                 <li><Link to="/retail" className="block px-5 py-2 hover:bg-gray-200">Retail</Link></li>
                                                 <li><Link to="/corporate" className="block px-5 py-2 hover:bg-gray-200">Corporate</Link></li>
                                                 <li><Link to="/vendor" className="block px-5 py-2 hover:bg-gray-200">Vendor</Link></li>
